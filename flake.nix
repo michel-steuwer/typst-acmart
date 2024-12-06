700226bfc61020ed403101e6509e261e7ccf687d
hylo <hylo@posteo.de>
2024-12-06 18:56:05 +0100

    ci: add workflow to generate diff.pdf with diff-pdf

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              typst
+              diff-pdf
+              libertine
+            ];
+            FONTCONFIG_FILE = makeFontsConf {
+              fontDirectories = [libertine];
+            };
+          };
+        }
+    );
+}
